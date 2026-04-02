29144f916bdb17f5d7d32ad0ef11d78ec6c9a3ec
Naitikagrawal01 <naitikgoyalsahabh@gmail.com>
2026-04-02 15:17:08 +0530

    atlas final

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // 🔥 FIREBASE FAIL HUA TOH LOCAL USE KAR
-mongoose.connect("mongodb://127.0.0.1:27017/kirana")
+mongoose.connect("mongodb+srv://webx:1234@cluster0.pt8c1mc.mongodb.net/kirana?retryWrites=true&w=majority")
 .then(() => console.log("MongoDB Connected ✅"))
 .catch(err => console.log(err));
 
